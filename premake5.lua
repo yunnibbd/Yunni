e6acd06a930c6d3fbd1cbfe6a2558cf2239bbb0b
yunni <yunnibbd@163.com>
2023-02-14 22:33:08 +0800

    add glm

--- premake5.lua
+++ premake5.lua
@@ -15,6 +15,7 @@ IncludeDir = {}
 IncludeDir["GLFW"] = "Yunni/vendor/GLFW/include"
 IncludeDir["Glad"] = "Yunni/vendor/Glad/include"
 IncludeDir["ImGui"] = "Yunni/vendor/imgui"
+IncludeDir["glm"] = "Yunni/vendor/glm"
 
 include "Yunni/vendor/GLFW"
 include "Yunni/vendor/Glad"
@@ -35,7 +36,9 @@ project "Yunni"
 	files
 	{
 		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl",
 	}
 
 	includedirs
@@ -44,7 +47,8 @@ project "Yunni"
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
 		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
+		"%{IncludeDir.ImGui}",
+		"%{IncludeDir.glm}"
 	}
 
 	links
@@ -104,7 +108,8 @@ project "Sandbox"
 	includedirs
 	{
 		"Yunni/vendor/spdlog/include",
-		"Yunni/src"
+		"Yunni/src",
+		"%{IncludeDir.glm}"
 	}
 
 	links
