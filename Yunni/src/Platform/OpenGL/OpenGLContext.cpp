3941ecb676dd078d0cb6de504804a4b8eb39ff16
yunni <yunnibbd@163.com>
2023-02-16 23:29:11 +0800

    First Triangle

--- Yunni/src/Platform/OpenGL/OpenGLContext.cpp
+++ Yunni/src/Platform/OpenGL/OpenGLContext.cpp
@@ -3,6 +3,7 @@
 
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <GL/GL.h>
 
 namespace Yunni {
 	OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
@@ -16,6 +17,11 @@ namespace Yunni {
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		YN_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		YN_CORE_INFO("OpenGL Info:");
+		YN_CORE_INFO(" Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+		YN_CORE_INFO(" Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		YN_CORE_INFO(" Version: {0}", (const char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
--- Yunni/src/Yunni/Log.h
+++ Yunni/src/Yunni/Log.h
@@ -6,6 +6,7 @@
 // This ignores all warnings raised inside External headers
 #pragma warning(push, 0)
 #include <spdlog/spdlog.h>
+#include <spdlog/fmt/ostr.h>
 #pragma warning(pop)
 
 namespace Yunni {
