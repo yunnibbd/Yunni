a6d12258760b78d24508d16ba36e8dc78615ff00
yunni <yunnibbd@163.com>
2023-02-22 22:10:27 +0800

    Vertex Buffer Layouts

--- Yunni/src/Platform/OpenGL/OpenGLBuffer.h
+++ Yunni/src/Platform/OpenGL/OpenGLBuffer.h
@@ -9,11 +9,16 @@ namespace Yunni {
 		OpenGLVertexBuffer(float* vertices, uint32_t size);
 		virtual ~OpenGLVertexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
+
+
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
 
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer
@@ -22,8 +27,8 @@ namespace Yunni {
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 
 		virtual uint32_t GetCount() const { return m_Count; }
 	private:
--- Yunni/src/Yunni/Application.cpp
+++ Yunni/src/Yunni/Application.cpp
@@ -12,6 +12,27 @@ namespace Yunni {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+			case Yunni::ShaderDataType::None:		return GL_FLOAT;
+			case Yunni::ShaderDataType::Float:		return GL_FLOAT;
+			case Yunni::ShaderDataType::Float2:		return GL_FLOAT;
+			case Yunni::ShaderDataType::Float3:		return GL_FLOAT;
+			case Yunni::ShaderDataType::Float4:		return GL_FLOAT;
+			case Yunni::ShaderDataType::Mat3:		return GL_FLOAT;
+			case Yunni::ShaderDataType::Mat4:		return GL_FLOAT;
+			case Yunni::ShaderDataType::Int:		return GL_INT;
+			case Yunni::ShaderDataType::Int2:		return GL_INT;
+			case Yunni::ShaderDataType::Int3:		return GL_INT;
+			case Yunni::ShaderDataType::Int4:		return GL_INT;
+			case Yunni::ShaderDataType::Bool:		return GL_BOOL;
+		}
+		YN_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		YN_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -27,16 +48,38 @@ namespace Yunni {
 		glGenVertexArrays(1, &m_VertexArray);
 		glBindVertexArray(m_VertexArray);
 
-		float vertices[3 * 3] = {
-			-0.5f, -0.5f, 0.0f,
-			0.5f, -0.5f, 0.0f,
-			0.0f, 0.5f, 0.0f
+		float vertices[3 * 7] = {
+			-0.5f, -0.5f, 0.0f, 0.8f, 0.2f, 0.8f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 0.2f, 0.3f, 0.8f, 1.0f,
+			 0.0f,  0.5f, 0.0f, 0.8f, 0.8f, 0.2f, 1.0f
 		};
 
 		m_VertexBuffer.reset(VertexBuffer::Create(vertices, sizeof(vertices)));
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, nullptr);
+		{
+			BufferLayout layout = {
+			{ ShaderDataType::Float3, "a_Position" },
+			{ ShaderDataType::Float4, "a_Color" },
+			};
+
+			m_VertexBuffer->SetLayout(layout);
+		}
+
+		uint32_t index = 0;
+		const auto& layout = m_VertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(
+				index, 
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				layout.GetStride(),
+				(const void*)element.Offset
+			);
+			++index;
+		}
 
 		uint32_t indices[3] = {0, 1, 2};
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
@@ -45,12 +88,15 @@ namespace Yunni {
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;			
+			layout(location = 1) in vec4 a_Color;
 
 			out vec3 v_Position;
+			out vec4 v_Color;
 
 			void main()
 			{
 				v_Position = a_Position;
+				v_Color = a_Color;
 				gl_Position = vec4(a_Position, 1.0);
 			}
 		)";
@@ -61,10 +107,12 @@ namespace Yunni {
 			layout(location = 0) out vec4 color;			
 
 			in vec3 v_Position;
+			in vec4 v_Color;
 
 			void main()
 			{
-				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				//color = vec4(v_Position * 0.5 + 0.5, 1.0);
+				color = v_Color;
 			}
 		)";
 
