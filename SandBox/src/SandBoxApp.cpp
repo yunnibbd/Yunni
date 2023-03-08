c1e1530467aef35ffc4e166de674c43cd1c0289d
yunni <yunnibbd@163.com>
2023-03-08 22:03:38 +0800

    Transforms

--- SandBox/src/SandBoxApp.cpp
+++ SandBox/src/SandBoxApp.cpp
@@ -2,6 +2,8 @@
 
 #include "ImGui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Yunni::Layer
 {
 public:
@@ -37,10 +39,10 @@ public:
 		m_SquareVA.reset(Yunni::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f,
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f,
 		};
 
 		std::shared_ptr<Yunni::VertexBuffer> squareVB;
@@ -62,6 +64,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -70,7 +73,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -97,13 +100,14 @@ public:
 			layout(location = 0) in vec3 a_Position;			
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -127,7 +131,7 @@ public:
 	{
 		YN_TRACE("Delta time: {0}s ({1}ms)", ts.GetSeconds(), ts.GetMilliseconds());
 		float  time = ts;
-		if (Yunni::Input::IsKeyPr`essed(YN_KEY_LEFT))
+		if (Yunni::Input::IsKeyPressed(YN_KEY_LEFT))
 			m_CameraPosition.x -= m_CameraMoveSpeed * ts;
 		else if (Yunni::Input::IsKeyPressed(YN_KEY_RIGHT))
 			m_CameraPosition.x += m_CameraMoveSpeed * ts;
@@ -151,7 +155,16 @@ public:
 
 		Yunni::Renderer::BeginScene(m_Camera);
 
-		Yunni::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; ++y) {
+			for (int x = 0; x < 20; ++x) {
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				Yunni::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+		
 		Yunni::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Yunni::Renderer::EndScene();
