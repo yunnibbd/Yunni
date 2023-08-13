791f468f764c4ebf3e732b561f86d4bb18df14e5
yunni <yunnibbd@163.com>
2023-08-13 17:19:49 +0800

    2D Renderer Textures

--- SandBox/src/SandBox2D.cpp
+++ SandBox/src/SandBox2D.cpp
@@ -8,12 +8,12 @@
 Sandbox2D::Sandbox2D()
 	:Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f)
 {
-
+	
 }
 
 void Sandbox2D::OnAttach()
 {
-	
+	m_Texture = Yunni::Texture2D::Create("assets/textures/mps4.png");
 }
 
 void Sandbox2D::OnDetach()
@@ -33,6 +33,7 @@ void Sandbox2D::OnUpdate(Yunni::Timestep ts)
 
 	Yunni::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 	Yunni::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
+	Yunni::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_Texture);
 	Yunni::Renderer2D::EndScene();
 }
 
--- Yunni/src/Yunni/Renderer/Renderer2D.h
+++ Yunni/src/Yunni/Renderer/Renderer2D.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "OrthographicCamera.h"
 
+#include "Texture.h"
+
 namespace Yunni {
 	class Renderer2D
 	{
@@ -14,5 +16,7 @@ namespace Yunni {
 		//Primitives
 		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color);
 		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color);
+		static void DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D> texture);
+		static void DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D> texture);
 	};
 }
