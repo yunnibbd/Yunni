6e0ddc2df7959958176f89f768828b42dc3ccd4f
yunni <yunnibbd@163.com>
2023-09-06 23:41:13 +0800

    Single Shader 2D Renderer

--- Yunni/src/Platform/OpenGL/OpenGLTexture.h
+++ Yunni/src/Platform/OpenGL/OpenGLTexture.h
@@ -2,15 +2,20 @@
 
 #include "Yunni/Renderer/Texture.h"
 
+#include <glad/glad.h>
+
 namespace Yunni {
 	class OpenGLTexture2D : public Texture2D
 	{
 	public:
+		OpenGLTexture2D(uint32_t width, uint32_t height);
 		OpenGLTexture2D(const std::string& path);
 		virtual ~OpenGLTexture2D();
 
 		uint32_t GetWidth() const override { return m_Width; }
 		uint32_t GetHeight() const override { return m_Height; }
+
+		virtual void SetData(void* data, uint32_t size) override;
 
 		void Bind(uint32_t slot = 0) const override;
 
@@ -18,5 +23,6 @@ namespace Yunni {
 		std::string m_Path;
 		uint32_t m_Width, m_Height;
 		uint32_t m_RendererID;
+		GLenum m_InternalFormat, m_DataFormat;
 	};
 }
--- Yunni/src/Yunni/Renderer/Texture.cpp
+++ Yunni/src/Yunni/Renderer/Texture.cpp
@@ -5,6 +5,21 @@
 #include "Platform/OpenGL/OpenGLTexture.h"
 
 namespace Yunni {
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t  height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			YN_CORE_ASSERT(false, "RendererAPI::API::None is currently not support!");
+			break;
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLTexture2D>(width, height);
+		}
+
+		YN_CORE_ASSERT(false, "Unknow RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch(Renderer::GetAPI())
@@ -13,7 +28,7 @@ namespace Yunni {
 			YN_CORE_ASSERT(false, "RendererAPI::API::None is currently not support!");
 			break;
 		case RendererAPI::API::OpenGL:
-			return std::make_shared<OpenGLTexture2D>(path);
+			return CreateRef<OpenGLTexture2D>(path);
 		}
 
 		YN_CORE_ASSERT(false, "Unknow RendererAPI!");
